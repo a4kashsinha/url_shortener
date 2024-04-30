3b00e8b02f28649ad9ee7be6f2b91a92924f7a8d
a4kashsinha <aakashs67@yahoo.com>
2024-04-30 09:57:36 -0500

    Integrated short URL functionality, still pending MongoDB download. Website is DOWN.

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const ShortUrl = require('./models/shortUrl')
 const app = express()
 
-mongoose.connect('mongoDB:localhost/urlShortener', {
-    userNewUrlParser: true, useUnifiedTopology: true
-})
+mongoose.connect('mongodb://localhost/urlShortener', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req, res) => {
     res.render('index')
 })
 
-app.post('/shortURLs', (req, res) +> {
-
+app.post('/shortURLs', async(req, res) => {
+    await ShortUrl.create({ full: req.body.fullUrl })
+    res.redirect('/')
 })
 app.listen(process.env.PORT || 5000)
