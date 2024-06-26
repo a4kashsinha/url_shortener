cb60708ead9d7160c1c9e33ccdee300a91f473fb
a4kashsinha <aakashs67@yahoo.com>
2024-06-26 07:58:54 -0500

    DEBUGGED shortURL functionality. Working as intended

--- server.js
+++ server.js
@@ -21,4 +21,15 @@ app.post('/shortURLs', async(req, res) => {
     await ShortUrl.create({ full: req.body.fullUrl })
     res.redirect('/')
 })
+
+app.get('/:shortUrl', async (req, res) => {
+    const shortUrl = await ShortUrl.findOne ({short: req.params.shortUrl })
+    if (shortUrl == null) return res.sendStatus(404)
+
+    shortUrl.clicks++
+    shortUrl.save()
+
+    res.redirect(shortUrl.full)
+})
+
 app.listen(process.env.PORT || 5000)
